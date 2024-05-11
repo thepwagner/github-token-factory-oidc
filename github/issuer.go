04f1ff39012d573dff93de25210492a506304d50
Peter Wagner <1559510+thepwagner@users.noreply.github.com>
2024-05-11 07:35:51 -0400

    github v62

--- github/issuer.go
+++ github/issuer.go
@@ -6,7 +6,7 @@ import (
 	"log/slog"
 	"strings"
 
-	"github.com/google/go-github/v61/github"
+	"github.com/google/go-github/v62/github"
 	"github.com/thepwagner/github-token-factory-oidc/api"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
